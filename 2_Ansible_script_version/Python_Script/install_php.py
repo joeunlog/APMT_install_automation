ff828f78e479133b428f24fa6cd0cf664969567e
james5085 <james6852311@gmail.com>
2021-11-04 18:28:27 +0900

    php_python script 작성

--- 2_Ansible_script_version/Python_Script/install_php.py
+++ 2_Ansible_script_version/Python_Script/install_php.py
@@ -12,12 +12,24 @@ if len(result) == 0:
     subprocess.call('yum -y install wget', shell=True)
     
 subprocess.call('ls -l', shell=True)
-subprocess.call('yum -y install libxml2-devel openssl-devel libjpeg-devel libpng-devel vim', shell=True)
+subprocess.call('cd /usr/local/src')
+subprocess.call('yum -y install wget libxml2-devel openssl-devel libjpeg-devel libpng-devel vim', shell=True)
 
-php_version=input('which version you going to install? : ')
-print('Php version = ', php_version, 'correct? (yes/no)')
-user_ans=input(':')
-if user_ans == 'yes':
-    urlphp = 'https://www.php.net/distributions/php-'+php_version+'.tar.gz'
-    print(urlphp)
-    subprocess.call(['wget ' ,print(urlphp)], shell=True)
+while True:
+    php_version = 0 , user_ans = 0, urlphp = 0
+    php_version=input('which version you going to install? : ')
+    print('Php version ={}'.format(php_version), 'correct? (yes/no)')
+    user_ans=input(':')
+    if user_ans == 'yes':
+        urlphp = 'wget https://www.php.net/distributions/php-{}.tar.gz'.format(php_version)
+        print(urlphp)
+        subprocess.Popen(urlphp.split(), stdout=subprocess.PIPE )
+        break
+    if user_ans != 'yes':
+        print('plz type your version again')
+php_dir = 'php-{}'.format(php_version)
+subprocess.Popen(['tar {}'.format(php_dir), '-zxvf'], stdout=subprocess.PIPE )
+subprocess.Popen(['cd', php_dir.split()])
+subprocess.Popen(['./configure', '--prefix=/usr/local/src/php','--with-apxs=/usr/local/apache/bin/apxs', '--with-mysql --with-mysqli', '--with-zlib', '--with-jpeg' ,'--with-png', '--with-openssl', '--with-libxml', '--with-iconv', '--with-gd', '--enable-curl', '--enable-sockets', '--disable-fileinfo', '--disable-debug' ])
+subprocess.call('make')
+subprocess.call('make install')
--- 2_Ansible_script_version/Python_Script/test.py
+++ 2_Ansible_script_version/Python_Script/test.py
@@ -3,11 +3,7 @@ print("## key값 보내는 방법 조사 시작합니다. - 연수 - ")
 print("mySQL 설치 자료 조사 시작합니다.")
 print("test test")
 
-php_version=input('which version you going to install? : ')
-print('Php version = ', php_version, 'correct? (yes/no)')
-user_ans=input(':')
-if user_ans == 'yes':
-    url_php = 'https://www.php.net/distributions/php-'+php_version+'.tar.gz'
-    print(url_php)
+php_version='7.3.32'
+com_php_dir = 'php'+'-'+php_version+'.tar.gz'
 
-    
+print(com_php_dir)
